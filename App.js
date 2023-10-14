737840e1367504076f257ce3345f5ee4863346f0
zzhao003 <zzhao003@gmail.com>
2023-10-13 21:35:51 -0400

    adds homepage with input func

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredText(enteredText);
+  };
+  const addGoalHandler = () => {
+    setGoals((currentGoals) => [
+      ...currentGoals,
+      { text: enteredText, id: Math.random().toString() },
+    ]);
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your Goal!" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalContainer}>
-        <Text>List of Goals...</Text>
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goal}>
+                <Text>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        ></FlatList>
       </View>
     </View>
   );
@@ -39,5 +76,12 @@ const styles = StyleSheet.create({
   },
   goalContainer: {
     flex: 5,
+  },
+  goal: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "lightblue",
+    // color: "white",
   },
 });
